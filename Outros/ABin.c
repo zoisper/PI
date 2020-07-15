9bae8a26ba1be43fd14fdb11be14d951c7c0ddd9
tiago <you@example.com>
2020-07-15 23:35:09 +0100

    wip

--- Outros/ABin.c
+++ Outros/ABin.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct abin {
 int valor;
@@ -8,35 +7,32 @@ struct abin *esq,
 *dir;
 } *ABin;
 
-ABin fromArray (int v[], int n)  /// cria arvore binaria de array
+ABin ABinfromArray (int v[], int N)  // cria arvore binaria apartir de array
 {
 
 	ABin r = NULL;
 	if(n>0)
 	{
 		r = malloc (sizeof (struct abin));
- 		r->valor = v[n/2];
- 		r->esq = fromArray (v, n/2);
- 		r->dir = fromArray (v+(n/2)+1, n - n/2 -1);
+ 		r->valor = v[N/2];
+ 		r->esq = ABinfromArray (v, N/2);
+ 		r->dir = ABinfromArray (v+ N/2 +1, N - N/2 - 1);
 	}
-
 return r;
-
 }
 
-void showABin (ABin a)  /////////////////////// imprime ABin
+void showABin (ABin a)  // imprime ABin
 {
 	if (a)
 	{
 		showABin (a->esq);
 		printf("%d\n",a->valor );
 		showABin (a->dir);
-
 	}
 }
 
 
-void showPathsAux (ABin a, char *c)
+void showPathsAux (ABin a, char *c) // função auxiliar para imprimir caminhos de arvore binaria
 {
 	if(a)
 	{
@@ -49,52 +45,81 @@ void showPathsAux (ABin a, char *c)
 		strcat (d, ">");
 		showPathsAux (a->esq, e);
 		showPathsAux (a->dir, d);
-
 	}
 } 
 
-void showPaths (ABin a)  //// imprime caminhos de arvore binaria
+void showPaths (ABin a)  // imprime caminhos de arvore binaria
 {
 	if (a)
 	showPathsAux (a,"");
 }
 
 
-void ABintoDL (ABin *a)   ////converte ABin em lista duplamente ligada sem malloc
+void DLFromAbin (ABin *a)   // converte ABin em lista duplamente ligada sem malloc
 {
 
 	if (*a)
 	{
-		ABintoDL (&((*a)->esq));
-		ABintoDL (&((*a)->dir));
-
+		DLFromAbin (&((*a)->esq));
+		DLFromAbin (&((*a)->dir));
 		if ((*a)->dir)
 			(*a)->dir->esq = (*a);
-
 		if ((*a)->esq)
 		{
 			ABin aux = ((*a)->esq);
-			ABin r = ((*a)->esq);
-			
+			ABin r = ((*a)->esq);	
 			while (aux->dir)
-				aux = aux->dir;
-			
+				aux = aux->dir;	
 			aux->dir = (*a);
 			(*a)->esq = aux;
-
 			while (r->esq)
 				r = r->esq;
-
 			(*a) = r;
-		}
-
-		
+		}	
 	}
 }
 
+int lenDL (ABin a) // calcula comprimento de lista duplamente ligada
+{
+    int r = 0;
+    while (a)
+    {
+        r++;
+        a = a->dir;
+    }
+    return r;
+}
 
+void ABinFromDL (ABin *a) // converte lista duplamente ligada em ABin sem malloc
+{
+    ABin raiz = *a, e = NULL;
+    if (*a)
+    {
+        int len = lenDL (*a);
+        len /= 2;
+        while (len > 0)
+        {
+            raiz = raiz->dir;
+            len--;
+        }
+        *a = raiz;
+        e = (*a)->esq;
+        if (e)
+        {
+            e->dir = NULL;
+            while (e->esq)
+                e = e->esq;
+            (*a)->esq = e;
+        }
+        if ((*a)->dir)
+            (*a)->dir->esq = NULL;
+        
+        ABinFromDL(&((*a)->esq));
+        ABinFromDL(&((*a)->dir));
+    }
+}
 
-void showABinDL (ABin a)  //////////  imprime ABin convertida em DLint sem malloc
+void showDL (ABin a)  // imprime ABin convertida em lista duplamente ligada
 {
 	while (a)
 	{
@@ -103,7 +128,7 @@ void showABinDL (ABin a)  //////////  imprime ABin convertida em DLint sem mallo
 	}
 }
 
-void showABinDLBack (ABin a)  /////////////  imprime ABin convertida em DLInt sem malloc ao contrario
+void showDLBack (ABin a)  // imprime ABin convertida em em lista duplamente ligada ao contrario (para testar se a conversão foi bem sucedida)
 {
 	while (a && a->dir)
 		a = a->dir;
@@ -112,4 +137,24 @@ void showABinDLBack (ABin a)  /////////////  imprime ABin convertida em DLInt se
 		printf("%d\n", a->valor );
 		a = a->esq;
 	}
+}
+
+
+int main ()
+{
+    int v[10] = {0,1,2,3,4,5,6,7,8,9};
+    ABin a = ABinFromArray (v, 10);
+    showABin (a);
+    putchar ('\n');
+    DLFromAbin (&a);
+    showDL (a);
+    putchar ('\n');
+    showDLBack (a);
+    putchar ('\n');
+    ABinFromDL (&a);
+    putchar ('\n');
+    showABin (a);
+    
+    
+    return 0;
 }
